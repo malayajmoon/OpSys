4b95d30cc5a4f5ff68ad30c36cd890e5cfe99c7e
Malaya Moon <56135961+malayajmoon@users.noreply.github.com>
2022-12-13 13:40:28 -0500

    Lab 1 Linked Lists

---
+++ Lab 1/Makefile
@@ -0,0 +1,7 @@
+# list/Makefile
+#
+# Makefile for list implementation and test file.
+#
+# <Author> Malaya Moon
+list: main.c list.c
+	gcc list.h list.c main.c -o main 
--- Lab 1/main.c
+++ Lab 1/main.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "list.h"
 
+int main() 
+{
+  printf("Write your Tests for your linked list implementation\n");
+  
+  //list_t *mylist;
+  
+  //mylist = list_alloc();
+  
+  return 0;
+ 
+}
